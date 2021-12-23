3281b0cb011f494dab886f986a6caa9e0e9f3836
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2021-12-23 10:00:47 +0530

    added:Latest File

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -1,6 +1,20 @@
+
+import java.util.Random;
 public class EmployeeWageBuilder {
     public  static void  main(String[] args)
     {
-        System.out.println("Employee wage");
+        System.out.println("Welcome To Employee Wage Builder");
+        int IS_PRESENT=1;
+        Random random=new Random();
+        int empCheck=random.nextInt(2);
+        if(empCheck==IS_PRESENT)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is absent");
+
+        }
     }
 }

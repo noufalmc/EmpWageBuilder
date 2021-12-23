cdaeea58ba8d45b97c77d74629e6c842d04c9ed3
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2021-12-23 10:05:27 +0530

    added:Wages Calculated

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -5,10 +5,14 @@ public class EmployeeWageBuilder {
     {
         System.out.println("Welcome To Employee Wage Builder");
         int IS_PRESENT=1;
+        int PER_RATE_HOUR=80;
+        int EMP_HOUR=0;
+        int EMP_WAGES=0;
         Random random=new Random();
         int empCheck=random.nextInt(2);
         if(empCheck==IS_PRESENT)
         {
+            EMP_HOUR=8;
             System.out.println("Employee is Present");
         }
         else
@@ -16,5 +20,7 @@ public class EmployeeWageBuilder {
             System.out.println("Employee is absent");
 
         }
+        EMP_WAGES=EMP_HOUR*PER_RATE_HOUR;
+        System.out.println("Daily Wages is"+EMP_WAGES);
     }
 }

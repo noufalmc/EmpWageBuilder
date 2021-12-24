657b7d64581962cec6d4227d9db14835c0b72c11
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2021-12-24 09:07:24 +0530

    latest

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -5,21 +5,26 @@ public class EmployeeWageBuilder {
     {
         System.out.println("Welcome To Employee Wage Builder");
         int IS_PRESENT=1;
-        int PER_RATE_HOUR=80;
+        int IS_FULL_TIME=1;
+        int IS_PART_TIME=2;
+        int PER_RATE_HOUR=20;
         int EMP_HOUR=0;
         int EMP_WAGES=0;
         Random random=new Random();
-        int empCheck=random.nextInt(2);
-        if(empCheck==IS_PRESENT)
+        int empCheck=random.nextInt(3);
+        if(empCheck==IS_FULL_TIME)
         {
             EMP_HOUR=8;
-            System.out.println("Employee is Present");
+        }
+        else if(empCheck==IS_PART_TIME)
+        {
+            EMP_HOUR=4;
         }
         else
         {
-            System.out.println("Employee is absent");
-
+            System.out.println("Employye is Absent");
         }
+
         EMP_WAGES=EMP_HOUR*PER_RATE_HOUR;
         System.out.println("Daily Wages is"+EMP_WAGES);
     }

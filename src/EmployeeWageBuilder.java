d95654b4737402c341a24a9ed3293baad2002473
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2021-12-24 09:19:42 +0530

    Added:Switch Cases

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -1,30 +1,27 @@
 
 import java.util.Random;
 public class EmployeeWageBuilder {
+    public static final int IS_FULL_TIME=1;
+    public static final int IS_PART_TIME=2;
+    public static final int PER_RATE_HOUR=20;
     public  static void  main(String[] args)
     {
         System.out.println("Welcome To Employee Wage Builder");
-        int IS_PRESENT=1;
-        int IS_FULL_TIME=1;
-        int IS_PART_TIME=2;
-        int PER_RATE_HOUR=20;
         int EMP_HOUR=0;
         int EMP_WAGES=0;
         Random random=new Random();
         int empCheck=random.nextInt(3);
-        if(empCheck==IS_FULL_TIME)
+        switch (empCheck)
         {
-            EMP_HOUR=8;
+            case IS_FULL_TIME:
+                EMP_HOUR=8;
+                break;
+            case IS_PART_TIME:
+                EMP_HOUR=4;
+                break;
+            default:
+                EMP_HOUR=0;
         }
-        else if(empCheck==IS_PART_TIME)
-        {
-            EMP_HOUR=4;
-        }
-        else
-        {
-            System.out.println("Employye is Absent");
-        }
-
         EMP_WAGES=EMP_HOUR*PER_RATE_HOUR;
         System.out.println("Daily Wages is"+EMP_WAGES);
     }

b47e6c1a1dc5c026cf9eae7452330b78bde96268
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2021-12-24 17:27:55 +0530

    Added:Monthly Salary

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -5,26 +5,33 @@ public class EmployeeWageBuilder {
     public static final int IS_PART_TIME=2;
     public static final int PER_RATE_HOUR=20;
     public static final int TOTAL_WORKING_DAYS=20;
+    public static  final int TOTAL_WORKING_HOURS=100;
     public  static void  main(String[] args)
     {
         System.out.println("Welcome To Employee Wage Builder");
         int EMP_HOUR=0;
         int EMP_WAGES=0;
-        for(int i=0;i<TOTAL_WORKING_DAYS;i++) {
+        int i=0;
+        while( i<20 && EMP_HOUR<TOTAL_WORKING_HOURS)
+        {
             Random random = new Random();
             int empCheck = random.nextInt(3);
             switch (empCheck) {
                 case IS_FULL_TIME:
-                    EMP_HOUR+= 8;
+                    EMP_HOUR+=8;
                     break;
                 case IS_PART_TIME:
-                    EMP_HOUR+= 4;
+                    EMP_HOUR+=4;
                     break;
                 default:
-                    EMP_HOUR = 0;
+                    EMP_HOUR+=0;
             }
+            ++i;
+            System.out.println("EMP Hour"+empCheck);
         }
         EMP_WAGES=EMP_HOUR*PER_RATE_HOUR;
         System.out.println("Daily Wages is"+EMP_WAGES);
+        System.out.println("EMP Hour is"+EMP_HOUR);
+        System.out.println("Day  is"+i);
     }
 }

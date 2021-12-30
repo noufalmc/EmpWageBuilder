6b247c1d211322eacfc59a2bef381a671ba56fa7
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2021-12-30 20:49:30 +0530

    Added:OOPs Added

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -1,18 +1,19 @@
 
 import java.util.Random;
-public class EmployeeWageBuilder {
+class Employee
+{
     public static final int IS_FULL_TIME=1;
     public static final int IS_PART_TIME=2;
-    public static final int PER_RATE_HOUR=20;
+    public static final int PER_RATE_HOUR=100;
     public static final int TOTAL_WORKING_DAYS=20;
     public static  final int TOTAL_WORKING_HOURS=100;
-    public  static void  main(String[] args)
+    int EMP_HOUR=0;
+    int EMP_WAGE=0;
+    int I=0;
+    public void  CalcHour()
     {
-        System.out.println("Welcome To Employee Wage Builder");
-        int EMP_HOUR=0;
-        int EMP_WAGES=0;
-        int i=0;
-        while( i<20 && EMP_HOUR<TOTAL_WORKING_HOURS)
+
+        while( (I<20) && (EMP_HOUR<TOTAL_WORKING_HOURS))
         {
             Random random = new Random();
             int empCheck = random.nextInt(3);
@@ -26,12 +27,21 @@ public class EmployeeWageBuilder {
                 default:
                     EMP_HOUR+=0;
             }
-            ++i;
-            System.out.println("EMP Hour"+empCheck);
+            I++;
         }
-        EMP_WAGES=EMP_HOUR*PER_RATE_HOUR;
-        System.out.println("Daily Wages is"+EMP_WAGES);
-        System.out.println("EMP Hour is"+EMP_HOUR);
-        System.out.println("Day  is"+i);
+
+    }
+}
+public class EmployeeWageBuilder
+{
+    public  static void  main(String[] args)
+    {
+        System.out.println("Welcome To Employee Wage Builder");
+        Employee emp=new Employee();
+        emp.CalcHour();
+        System.out.println("MONTH Salary is = "+emp.EMP_HOUR*emp.PER_RATE_HOUR);
+        System.out.println("EMP Hour is"+emp.EMP_HOUR);
+        System.out.println("Day  is "+emp.I);
+
     }
 }

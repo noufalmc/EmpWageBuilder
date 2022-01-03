7f2a32efc7ce4eb96b3466c1af7ee76d408d6753
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2022-01-03 22:38:54 +0530

    Save Total Wage

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -1,47 +1,60 @@
 
 import java.util.Random;
-class Employee
-{
-    public static final int IS_FULL_TIME=1;
-    public static final int IS_PART_TIME=2;
-    public static final int PER_RATE_HOUR=100;
-    public static final int TOTAL_WORKING_DAYS=20;
-    public static  final int TOTAL_WORKING_HOURS=100;
-    int EMP_HOUR=0;
-    int EMP_WAGE=0;
-    int I=0;
-    public void  CalcHour()
-    {
 
-        while( (I<20) && (EMP_HOUR<TOTAL_WORKING_HOURS))
-        {
+class Emp {
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    private final String company;
+    private final int empRateHour;
+    private final int noOfWorkingDays;
+    private final int maxHourPerMonth;
+    private int totalWage;
+    public Emp(String CompanyName, int EmpRateHour, int noOfWorkingDays, int maxHourPerMonth) {
+        this.company = CompanyName;
+        this.empRateHour = EmpRateHour;
+        this.noOfWorkingDays = noOfWorkingDays;
+        this.maxHourPerMonth = maxHourPerMonth;
+    }
+
+    public void computeWage() {
+        int EMP_HOUR = 0;
+        int I = 0;
+        int DailyWage = 0;
+        totalWage=0;
+        while ((I < noOfWorkingDays) && (EMP_HOUR < maxHourPerMonth)) {
             Random random = new Random();
             int empCheck = random.nextInt(3);
             switch (empCheck) {
                 case IS_FULL_TIME:
-                    EMP_HOUR+=8;
+                    EMP_HOUR = 8;
                     break;
                 case IS_PART_TIME:
-                    EMP_HOUR+=4;
+                    EMP_HOUR = 4;
                     break;
                 default:
-                    EMP_HOUR+=0;
+                    EMP_HOUR = 0;
             }
             I++;
+            DailyWage =EMP_HOUR*empRateHour;
+            totalWage+= DailyWage;
         }
-
+    }
+    public String Tostring()
+    {
+        return "Company= "+company+" Total wage= "+totalWage;
     }
 }
-public class EmployeeWageBuilder
+class EmployeeWageBuilder
 {
     public  static void  main(String[] args)
     {
         System.out.println("Welcome To Employee Wage Builder");
-        Employee emp=new Employee();
-        emp.CalcHour();
-        System.out.println("MONTH Salary is = "+emp.EMP_HOUR*emp.PER_RATE_HOUR);
-        System.out.println("EMP Hour is"+emp.EMP_HOUR);
-        System.out.println("Day  is "+emp.I);
+        Emp obj1=new Emp("TCS",200,4,20);
+        obj1.computeWage();
+        System.out.println(obj1.Tostring());
+        Emp obj2=new Emp("Infosys",250,4,20);
+        obj2.computeWage();
+        System.out.println(obj2.Tostring());
 
     }
 }

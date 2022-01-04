d9eef3714dd75a4bd927556834e1ac6cf109302c
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2022-01-04 07:29:29 +0530

    Added:Multiple Companies Wage calculation

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -35,7 +35,7 @@ class Empinfo
     }
 
 }
-class Emp  implements IEmpinfo{
+class Emp  {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     private Empinfo[] empInfoArray;
